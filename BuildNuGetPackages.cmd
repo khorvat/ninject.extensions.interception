e5aeb2e140e565fd55b9b50e90322162de29db38
Kristijan Horvat <kristijan@mono.hr>
2014-12-10 16:05:51 +0100

    Upgraded to Ninject 3.2.2

--- BuildNuGetPackages.cmd
+++
@@ -1,5 +0,0 @@
-call tools\nuget\NuGet.exe pack "nuget/Ninject.Extensions.Interception.3.0.1-async02.nuspec" 
-call tools\nuget\NuGet.exe pack "nuget-linfu/Ninject.Extensions.Interception.Linfu.3.0.1-async02.nuspec" 
-
-move "Ninject.Extensions.Interception.3.0.1-async02.nupkg" \\buildserver\d$\Publish\NuGets\3rdParty\
-move "Ninject.Extensions.Interception.Linfu.3.0.1-async02.nupkg" \\buildserver\d$\Publish\NuGets\3rdParty\
